384227b38bd1ee2e06bcb46ee21cfdd65f901bca
Fatima <174022648+TehFat@users.noreply.github.com>
2024-09-16 18:30:13 +0200

    fist commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display');
+    function appendToDisplay(input){
+      display.value += input;
+    }
+    function clearDisplay(){
+      display.value = "";
+    }
+    function delOption(){
+      display.value = display.value.slice(0, -1); 
+    }
+    function calculate(){
+      try{
+        display.value = eval(display.value);
+      }
+      catch{ 
+      display.value = "Error";
+    }
+  }
